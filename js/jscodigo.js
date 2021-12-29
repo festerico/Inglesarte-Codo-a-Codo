b8cda3ae80efcd928a8156b026360a0656b48283
federico jose Bassi <72529292+festerico@users.noreply.github.com>
2021-12-29 18:30:19 -0300

    Add files via upload

--- js/jscodigo.js
+++ js/jscodigo.js
@@ -3,5 +3,5 @@
 let nombre= prompt("Por favor indicanos tu nombre y apellido:");
 
 let texto= document.getElementById("nombreDelVisitante");
-texto= document.write("<h3> Hola buenos dias  <h3>" + nombre);
+texto= document.write("<h3> Hola buenos dias  <h3>" + nombre + "<h3> te invitamos a recorrer nuestra pagina.  <h3>");
 

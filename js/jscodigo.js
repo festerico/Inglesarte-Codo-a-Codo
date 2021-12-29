af9ca67b888fb5a209b6f907dc098857ea388073
federico jose Bassi <72529292+festerico@users.noreply.github.com>
2021-12-29 18:26:19 -0300

    Add files via upload

---
+++ js/jscodigo.js
@@ -0,0 +1,7 @@
+//alert("Hola buenas, es un gusto para InglesArte tenerte por aqui, en la siguiente ventana te pediremos tu nombre y apellido para poder dirigirnos a ti.");
+
+let nombre= prompt("Por favor indicanos tu nombre y apellido:");
+
+let texto= document.getElementById("nombreDelVisitante");
+texto= document.write("<h3> Hola buenos dias  <h3>" + nombre);
+

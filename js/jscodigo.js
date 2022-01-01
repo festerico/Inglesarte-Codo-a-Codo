e636399f7ba02dcc379fc8f318bde5c7ff2bddc6
federico jose Bassi <72529292+festerico@users.noreply.github.com>
2022-01-01 16:31:47 -0300

    Add files via upload

--- js/jscodigo.js
+++ js/jscodigo.js
@@ -1,13 +1,18 @@
 //alert("Hola buenas, es un gusto para InglesArte tenerte por aqui, en la siguiente ventana te pediremos tu nombre y apellido para poder dirigirnos a ti.");
 
-let nombre= prompt("Por favor indicanos tu nombre y apellido:");
+alert(window.location.href.indexOf("index.html"))
+
+if(window.location.href.indexOf("index.html") > 1) {
+	let nombre= prompt("Por favor indicanos tu nombre y apellido:");
+};
+
+document.write(document.getElementById('nombreDelVisitante').innerhtml = " Hola buenos dias  " + nombre + " te invitamos a recorrer nuestra pagina");
 
 
-document.write(
-document.getElementById('nombreDelVisitante').innerhtml = " Hola buenos dias  " + nombre + " te invitamos a recorrer nuestra pagina");
 
 function ocultar(bOportunidades){
 	document.getElementById("demo").innerHTML = "PARA TENER MAS OPORTUNIDADES EN LA VIDA!"
-    var elemento = document.getElementById(bOportunidades);
-    elemento.style.display = "none";
+    document.getElementById(bOportunidades).style.display = "none";
+    document.getElementById("animacion").innerHTML="<h1 class=\"animate__animated animate__bounceInDown\">An animated element</h1>";
+
 }
